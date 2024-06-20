6b9a48e36ad755af40ed96c7346db8302e831bbd
atresnaprana <aditya.tresnaprana@nh2system.com>
2024-06-20 16:20:02 +0800

    add sql file

---
+++ testquery.sql
@@ -0,0 +1,6 @@
+CREATE table test(
+message varchar(255) NOT NULL)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO test(message) VALUES ('ANSIBLE TO DO LIST');
+INSERT INTO test(message) VALUES ('Get ready');
+INSERT INTO test(message) VALUES ('Ansible is fun');
