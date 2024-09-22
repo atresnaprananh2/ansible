32c726dfd8a70b0cfd3c37148d2837d64ffa0cb4
atresnaprananh2 <aditya.tresnaprana@nh2system.com>
2024-09-22 15:21:52 +0800

    added discovery script and completed postgre

--- pyscripts/testpy.py
+++ pyscripts/testpy.py
@@ -2,6 +2,10 @@ import os
 import re
 import xml.dom.minidom
 import tempfile
+import csv
+from pytwist import twistserver
+from pytwist.com.opsware.server import ServerRef
 
+ts = twistserver.TwistServer()
 
 print("Hello")
