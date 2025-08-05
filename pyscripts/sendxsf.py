eb83645cf867bc79e78f61c99e646219e2084c97
atresnaprananh2 <aditya.tresnaprana@nh2system.com>
2025-08-05 12:44:13 +0800

    changed to variable stored credential

--- pyscripts/sendxsf.py
+++ pyscripts/sendxsf.py
@@ -8,8 +8,12 @@ sa_core_ip = '172.17.40.33'              # SA Core IP
 remote_dir = '/var/tmp'                  # Remote path on SA Core
 
 # Prompt for SA Core credentials
-username = input("Enter SA Core username: ")
-_ = getpass.getpass("Enter SA Core password (will be prompted again by scp): ")  # Just a pause so user sees both prompts
+sa_user = input("Enter SA Core username: ")
+sa_pass = getpass.getpass("Enter SA Core password (will be prompted again by scp): ")
+
+# Now you can use sa_user and sa_pass later in your code (e.g., for API/PyTwist login)
+# print(sa_user)
+# print(sa_pass)
 
 # Loop through files and send via SCP
 for filename in os.listdir(xsf_dir):
@@ -17,13 +21,12 @@ for filename in os.listdir(xsf_dir):
         local_path = os.path.join(xsf_dir, filename)
         print(f"Uploading {filename}...")
 
-        # Construct SCP command (no sshpass)
+        # Construct SCP command (using static SCP user = root)
         scp_cmd = [
             'scp',
             local_path,
             f'root@{sa_core_ip}:{remote_dir}'
-            
         ]
 
-        # Run the command interactively
+        # Run the SCP command interactively (user will be prompted to input root password)
         subprocess.run(scp_cmd)
